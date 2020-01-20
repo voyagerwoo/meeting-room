fc3f5117bcb6514eb6b89850afa981e4546c68a8
voyagerwoo <voyager.woo@gmail.com>
2020-01-21 08:29:55 +0900

    codeguru profiler test

--- src/main/java/vw/meetingroom/MeetingRoomApplication.java
+++ src/main/java/vw/meetingroom/MeetingRoomApplication.java
@@ -3,12 +3,21 @@ package vw.meetingroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.codeguruprofilerjavaagent.Profiler;
 
 @SpringBootApplication
 @EnableCaching
 public class MeetingRoomApplication {
 
     public static void main(String[] args) {
+        Profiler systemProfiler =
+                Profiler.builder().profilingGroupName("codeguru-test")
+                        .awsCredentialsProvider(DefaultCredentialsProvider.create())
+                        .build();
+
+        systemProfiler.start();
+
         SpringApplication.run(MeetingRoomApplication.class, args);
     }
 
